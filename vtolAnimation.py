caac3d86ba868fde42fb8ce3d117bada346c5a9a
Andrew Torgesen <andrew.torgesen@gmail.com>
2019-11-12 22:03:07 -0500

    fix automatic animation and add obstacle visualization to simulator

--- vtolAnimation.py
+++ vtolAnimation.py
@@ -51,10 +51,14 @@ class vtolAnimation:
         self.handle = []                      # Initializes a list object that will
         self.padding = 0.5                    # be used to contain handles to the
                                               # patches and line objects.
+        self.obstacles = []
         plt.axis([-self.padding, P.L+self.padding, -self.padding, P.L]) # Change the x,y axis limits
 
         plt.plot([0, P.L],[0,0],'b-')         # Draw a base line
         plt.xlabel('z')
+        
+    def addObstacles(self, obstacles):
+        self.obstacles = obstacles
 
     def drawVtol(self, states):
         zv = states[0]
@@ -62,29 +66,38 @@ class vtolAnimation:
         theta = states[2]
 
         self.drawCopter(zv, h, theta)
+        self.drawObstacles()
         #self.ax.axis('equal') # This will cause the image to not distort
 
         # After each function has been called, initialization is over.
         if self.flagInit == True:
             self.flagInit = False
+            
+    def drawObstacles(self):
+        if self.flagInit == True:
+            for obstacle in self.obstacles:
+                w = obstacle[1][0] - obstacle[0][0]
+                h = obstacle[1][1] - obstacle[0][1]
+                self.handle.append(mpatches.Rectangle(obstacle[0], w, h, fc='green', ec='black'))
+                self.ax.add_patch(self.handle[-1])
 
     def drawCopter(self, zv, h, theta):
         if self.flagInit == True:
             self.copterArm = rotatingRectangle(self.ax, zv, h, theta, 2*P.d, P.caw)
             self.handle.append(self.copterArm.mpatch_obj)
-            self.ax.add_patch(self.handle[0])
+            self.ax.add_patch(self.handle[-1])
 
             self.copterBody = rotatingRectangle(self.ax, zv, h, theta, P.cbw, P.cbw)
             self.handle.append(self.copterBody.mpatch_obj)
-            self.ax.add_patch(self.handle[1])
+            self.ax.add_patch(self.handle[-1])
 
             self.leftRotor = rotatingEllipse(self.ax, zv-P.d*cos(theta), h-P.d*sin(theta), theta, P.rw, P.rh, fc_color='white')
             self.handle.append(self.leftRotor.mpatch_obj)
-            self.ax.add_patch(self.handle[2])
+            self.ax.add_patch(self.handle[-1])
 
             self.rightRotor = rotatingEllipse(self.ax, zv+P.d*cos(theta), h+P.d*sin(theta), theta, P.rw, P.rh, fc_color='white')
             self.handle.append(self.rightRotor.mpatch_obj)
-            self.ax.add_patch(self.handle[3])
+            self.ax.add_patch(self.handle[-1])
         else:
             self.copterArm.transform(zv, h, theta)
             self.copterBody.transform(zv, h, theta)
